0c4be3ac57bda894e767d10547462f2d169d5cfc
helene-cpu <66snahel@stud.akademiet.no>
2026-05-26 10:55:04 +0000

    Opprettet flere sider og ruter

--- app.py
+++ app.py
@@ -6,5 +6,21 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/bestill', methods=["POST", "GET"])
+def bestill():
+    return render_template('bestill.html')
+
+@app.route('/admin', methods=["POST", "GET"])
+def admin():
+    return render_template("admin.html")
+
+@app.route('/login', methods=["POST", "GET"])
+def login():
+    return render_template('login.html', methods=["POST", "GET"])
+
+@app.route('/register')
+def register():
+    return render_template('register.html', methods=["POST", "GET"])
+
 if __name__ == "__main__":
     app.run()
